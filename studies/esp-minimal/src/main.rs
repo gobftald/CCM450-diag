ef2f994d411dc33d9db1f897f70d2971c0230f9f
Ivancsics, Pal <gobftald@gmail.com>
2025-04-05 18:50:43 +0200

    starting to implement 'esp-minimal' to achieve a minimal size program capable to run on esp chips
    first we put #![no_std] into main.rs
    so the compiler immediately call for 'panic handler'
    
    we put a dummy handler into 'main.rs' which will be never called
    since we will define build-std-features = ["panic_immediate_abort"]
    
    to minimize code size we conifg "panic = 'abort'" as a deafult both
    for 'release' and 'debug' in the previous master .config.toml
    for further minimize code size we even eliminate all panic formatting
    with the mentioned "panic_immediate_abort" feature

--- studies/esp-minimal/src/main.rs
+++ studies/esp-minimal/src/main.rs
@@ -1,3 +1,14 @@
+#![no_std]
+
+// since we config 'build-std-features = ["panic_immediate_abort"]'
+// this handler will be never called, but the compiler insists on it
+#[panic_handler]
+fn panic(_: &core::panic::PanicInfo) -> ! {
+    loop {
+        unsafe { core::arch::asm!("wfi") }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    //println!("Hello, world!");
 }
