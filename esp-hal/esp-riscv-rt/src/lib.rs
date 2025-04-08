a58908d24f028debe5c4072689e7a42941913e79
Ivancsics, Pal <gobftald@gmail.com>
2025-04-08 13:56:56 +0200

    starting to implement esp runtime
    '.global _start' label/address is defined in esp-riscv-rt (lib.rs) assembly section
    _start will be defined as 'ENTRY(_start)' in a linker script defined later
    this is the 'start address' in elf object file (the compiled image) for which
    the second stage bootloader will jump as its last operation,
    so it is where everything is beginning for an application program

--- esp-hal/esp-riscv-rt/src/lib.rs
+++ esp-hal/esp-riscv-rt/src/lib.rs
@@ -1,3 +1,41 @@
 #![no_std]
 
+use core::arch::global_asm;
+
 pub use riscv_rt_macros::entry;
+
+/// Parse cfg attributes inside a global_asm call.
+// 303
+macro_rules! cfg_global_asm {
+    {@inner, [$($x:tt)*], } => {
+        global_asm!{$($x)*}
+    };
+    (@inner, [$($x:tt)*], #[cfg($meta:meta)] $asm:literal, $($rest:tt)*) => {
+        #[cfg($meta)]
+        cfg_global_asm!{@inner, [$($x)* $asm,], $($rest)*}
+        #[cfg(not($meta))]
+        cfg_global_asm!{@inner, [$($x)*], $($rest)*}
+    };
+    {@inner, [$($x:tt)*], $asm:literal, $($rest:tt)*} => {
+        cfg_global_asm!{@inner, [$($x)* $asm,], $($rest)*}
+    };
+    {$($asms:tt)*} => {
+        cfg_global_asm!{@inner, [], $($asms)*}
+    };
+}
+
+cfg_global_asm! {
+    r#"
+/*
+    Entry point of all programs (_start).
+
+    It initializes DWARF call frame information, the stack pointer, the
+    frame pointer (needed for closures to work in start_rust) and the global
+    pointer. Then it calls _start_rust.
+*/
+
+.section .init, "ax"
+.global _start
+
+"#,
+}
