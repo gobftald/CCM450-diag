b83514aa4071de242cefcc7dcf23732d61c2e60e
Ivancsics, Pal <gobftald@gmail.com>
2026-03-01 01:04:34 +0100

    deleting local portable-atomic crate

--- portable-atomic.me/src/gen/build.rs
+++
@@ -1,5 +0,0 @@
-#[allow(dead_code)]
-pub(crate) static NO_ATOMIC_CAS: &[&str] = &["riscv32imc-unknown-none-elf"];
-
-#[allow(dead_code)]
-pub(crate) static NO_ATOMIC_64: &[&str] = &["riscv32imc-unknown-none-elf"];
