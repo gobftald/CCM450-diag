485195b8e0a27ebe9f779a5d35b2da7e0c8d92af
Ivancsics, Pal <gobftald@gmail.com>
2026-03-02 22:04:18 +0100

    deleting local embassy-executor-macros, embassy-sync and embassy-time-driver crates

--- embassy/embassy-sync.me/src/waitqueue/mod.rs
+++
@@ -1,5 +0,0 @@
-mod atomic_waker;
-pub use atomic_waker::*;
-
-mod waker_registration;
-pub use waker_registration::*;
--- embassy/embassy-time-driver/src/tick.rs
+++
@@ -1,2 +0,0 @@
-#[cfg(feature = "tick-hz-1_000_000")]
-pub const TICK_HZ: u64 = 1_000_000;
