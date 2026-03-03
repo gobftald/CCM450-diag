6730fe81056888bd66ea3ecbda3fc1a32133f867
Ivancsics, Pal <gobftald@gmail.com>
2026-03-03 14:18:18 +0100

    deleting local esp-pacs crate, since esp-hal shows already its original crate

--- esp-pacs/esp32c3/src/rng/data.rs
+++
@@ -1,9 +0,0 @@
-/// Register `DATA` reader
-pub type R = crate::R<DATA_SPEC>;
-
-pub struct DATA_SPEC;
-impl crate::RegisterSpec for DATA_SPEC {
-    type Ux = u32;
-}
-
-impl crate::Readable for DATA_SPEC {}
--- esp-pacs/esp32c3/src/rtc_cntl/store4.rs
+++
@@ -1,9 +0,0 @@
-/// Register `STORE4` reader
-pub type R = crate::R<STORE4_SPEC>;
-
-pub struct STORE4_SPEC;
-impl crate::RegisterSpec for STORE4_SPEC {
-    type Ux = u32;
-}
-
-impl crate::Readable for STORE4_SPEC {}
