dc12cab88d188d04466982995462095576ded615
Ivancsics, Pal <gobftald@gmail.com>
2026-03-04 00:31:49 +0100

    deleting local esp-rtos crate
    we also need to modify esp-rtos in the forked esp-hal patch
    there are even more valuable traces in esp-rtos, so you can checkout before this commit

--- esp-hal/esp-rtos/src/esp_radio/queue.rs
+++
@@ -1,6 +0,0 @@
-use esp_radio_rtos_driver::{
-    queue::{CompatQueue, QueuePtr},
-    register_queue_implementation,
-};
-
-register_queue_implementation!(CompatQueue);
--- esp-hal/esp-rtos/src/task/xtensa.rs
+++
@@ -1,2 +0,0 @@
-// 19
-pub(crate) use esp_hal::trapframe::TrapFrame as CpuContext;
