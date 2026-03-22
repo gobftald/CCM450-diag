d3bcedc62c3eed07c21ca91102813187e60a9fd3
Ivancsics, Pal <gobftald@gmail.com>
2026-03-22 10:41:29 +0100

    Improving UDP server error handling

--- studies/ecu-via-elm327/src/udp.rs
+++ studies/ecu-via-elm327/src/udp.rs
@@ -1,5 +1,5 @@
 use embassy_futures::select::{Either, select};
-use embassy_net::udp::UdpMetadata;
+use embassy_net::udp::{RecvError, SendError, UdpMetadata, UdpSocket};
 
 // we can use NoopRawMutex since we use channel between two tasks in the same executor,
 // in single core environment and not using from interrupt
@@ -9,11 +9,12 @@ use embassy_sync::{
     zerocopy_channel::{Receiver, Sender},
 };
 
+const UDP_PORT: Option<&'static str> = option_env!("UDP_PORT");
+
 pub const UDP_BUFFER_SIZE: usize = crate::CHANNEL_ITEM_SIZE;
 const UDP_PACKET_MAX: usize = 4;
 
 #[cfg_attr(feature = "defmt", derive(defmt::Format))]
-#[derive(strum_macros::FromRepr)]
 pub enum Subsystem {
     // itself
     System,
@@ -21,12 +22,57 @@ pub enum Subsystem {
     Gps,
 }
 
-#[cfg_attr(feature = "defmt", derive(defmt::Format))]
-pub enum Reply {
-    InvalidSubystem,
+impl TryFrom<u8> for Subsystem {
+    type Error = ();
+
+    fn try_from(value: u8) -> Result<Self, Self::Error> {
+        match value {
+            0 => Ok(Self::System),
+            1 => Ok(Self::Ecu),
+            2 => Ok(Self::Gps),
+            _ => Err(()),
+        }
+    }
 }
 
-const UDP_PORT: Option<&'static str> = option_env!("UDP_PORT");
+#[cfg_attr(feature = "defmt", derive(defmt::Format))]
+pub enum Reply {
+    #[allow(dead_code)]
+    // RecvError: 0 = Truncated,
+    UdpRecvError(RecvError),
+
+    // SendError: 0 = NoRoute, 1 = SocketNotBound, 2 = PacketTooLarge,
+    UdpSendError(SendError),
+
+    InvalidSubsystem,
+}
+
+#[repr(u8)]
+pub enum ReplyCode {
+    UdpRecvError = 0x01,
+    UdpSendError = 0x02,
+    InvalidSubsystem = 0x03,
+}
+
+impl Reply {
+    pub fn code(&self) -> u8 {
+        match self {
+            Self::UdpRecvError(_) => ReplyCode::UdpRecvError as u8,
+            Self::UdpSendError(_) => ReplyCode::UdpSendError as u8,
+            Self::InvalidSubsystem => ReplyCode::InvalidSubsystem as u8,
+        }
+    }
+
+    pub fn sub_code(&self) -> u8 {
+        match self {
+            // RecvError: 0 = Truncated,
+            Self::UdpRecvError(err) => *err as u8,
+            // SendError: 0 = NoRoute, 1 = SocketNotBound, 2 = PacketTooLarge,
+            Self::UdpSendError(err) => *err as u8,
+            Self::InvalidSubsystem => ReplyCode::InvalidSubsystem as u8,
+        }
+    }
+}
 
 #[embassy_executor::task()]
 pub async fn server(
@@ -42,7 +88,7 @@ pub async fn server(
     let mut rx_buffer = [0; UDP_BUFFER_SIZE * UDP_PACKET_MAX];
     let mut tx_buffer = [0; UDP_BUFFER_SIZE * UDP_PACKET_MAX];
 
-    let mut socket = embassy_net::udp::UdpSocket::new(
+    let mut socket = UdpSocket::new(
         ap_stack,
         &mut rx_meta,
         &mut rx_buffer,
@@ -59,75 +105,105 @@ pub async fn server(
 
     loop {
         // wait for the channel to clear
-        let request_item = uart_sender.send().await;
+        let request = uart_sender.send().await;
 
-        // waiting for request or response
+        // waiting for udp request or uart response
         match select(
-            socket.recv_from(&mut request_item.data),
+            socket.recv_from(&mut request.data),
             uart_receiver.receive(),
         )
         .await
         {
             // UDP request arrived
             Either::First(result) => {
-                let (n, ep) = unwrap!(result);
+                match result {
+                    Ok((n, ep)) => {
+                        trace!(
+                            "#### UDP: socket.recv_from(): {}",
+                            request.data[..n]
+                        );
 
-                trace!(
-                    "#### UDP: socket.recv_from(): {}",
-                    request_item.data[..n]
-                );
+                        // first byte is the id of requested subsystem
+                        if let Ok(subsystem) = Subsystem::try_from(request.data[0] as u8) {
+                            match subsystem {
+                                Subsystem::System => {}
+                                Subsystem::Ecu => {
+                                    // send all icoming data
+                                    request.size = n;
 
-                if let Some(subsystem) = Subsystem::from_repr(request_item.data[0] as usize) {
-                    match subsystem {
-                        Subsystem::System => {}
-                        Subsystem::Ecu => {
-                            // send all icoming data
-                            request_item.size = n;
+                                    // send/forward request to uart
+                                    uart_sender.send_done();
+                                }
+                                Subsystem::Gps => {}
+                            }
+                        } else {
+                            // reply error by System: InvalidSubystem
+                            send_to(
+                                &mut socket,
+                                &[Subsystem::System as u8, Reply::InvalidSubsystem.code()],
+                                Some(ep)
+                                )
+                                .await;
 
-                            // forward request to uart
-                            uart_sender.send_done();
+                            debug!("#### UDP: send_to() returned");
                         }
-                        Subsystem::Gps => {}
-                    }
-                } else {
-                    unwrap!(
-                        socket
-                        // error sending subsystem is the system itself, which is 0 as u8
-                        .send_to(&[Subsystem::System as u8, Reply::InvalidSubystem as u8], ep)
-                        .await
-                    );
-                    debug!("#### UDP: socket.send_to() returned");
-                }
 
-                end_point = Some(ep);
+                        end_point = Some(ep);
+                    },
+
+                    Err(err) => {
+                        let err_buf = [Subsystem::System as u8, ReplyCode::UdpRecvError as u8, err as u8];
+                        send_to(&mut socket, &err_buf, end_point).await;
+                    }
+                }
             }
 
             // Uart answer arrived
-            Either::Second(received_item) => {
+            Either::Second(response) => {
                 trace!(
                     "#### UDP: uart_receiver.receive(): size: {}",
-                    received_item.size
+                    response.size
                 );
 
                 // forward response via UDP
-                if let Some(end_point) = end_point {
-                    unwrap!(
-                        socket
-                        .send_to(&received_item.data[..received_item.size], end_point)
-                        .await
-                    );
-                    debug!("#### UDP: socket.send_to() returned");
-                }
+                send_to(&mut socket, &response.data[..response.size], end_point).await;
+
+                debug!("#### UDP: send_to() returned");
 
                 uart_receiver.receive_done();
             }
         };
 
-        /*
-        //needs --features=esp-alloc/internal-heap-stats
-        unsafe {
-            debug!("{}", esp_alloc::HEAP.stats());
-        }
-        */
     }
+}
+
+// Calling a subfunction is almost always better than inlining the code into every error branch.
+// Since error handling usually happens in multiple places but shouldn't trigger often, you
+// want to optimize for Flash (code space) and Readability rather than raw execution speed.
+//
+// RAM
+// The compiler treats these calls as a Union, the Task's storage struct only reserves one slot.
+// The RAM footprint of the task does not increase beyond the size needed for a single call.
+//
+// Flash
+// This function exists once in the binary.
+// Each call is just a small bit of "glue" code to poll it.
+#[allow(unused_must_use)]
+async fn send_to(socket: &mut UdpSocket<'_>, buf: &[u8], end_point: Option<UdpMetadata>) {
+    // unwrapping end_point cannot panics
+    // UDP response cannot occur w/o a matching request
+    // so end_point as Option used instead of static mut
+    let end_point = unwrap!(end_point, "missing Endpoint");
+
+    if let Err(err) = socket.send_to(buf, end_point)
+        .await
+        .map_err(|err|
+            match err {
+                SendError::PacketTooLarge => Reply::UdpSendError(SendError::PacketTooLarge),
+                _ => panic!("UDP send error: {:?}", err),
+            })
+        {
+            let err_buf = [Subsystem::System as u8, err.code(), err.sub_code()];
+            socket.send_to(&err_buf, end_point).await;
+        }
 }
